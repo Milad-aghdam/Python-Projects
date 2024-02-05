f6995c59bdcc1bd208b22350128d136a036355f3
Milad <miladagdam@gmail.com>
2024-02-05 19:29:12 +0330

    adding a script for streamplit app

---
+++ level2/currency-convertor /src/app.py
@@ -0,0 +1,13 @@
+import streamlit as st
+from constants import CURRENCIES
+from currency_convertor import
+
+
+
+st.title(':dollar: Currency Converter')
+
+st.markdown("""
+This tool allows you to instantly convert amounts between different currencies 🌍.
+
+Enter the amount and choose the currencies to see the result.
+""")
