a36abdd1bae70357c1c979b06cab44886d1faef5
Milad <miladagdam@gmail.com>
2024-02-05 19:42:27 +0330

    adding v1

--- level2/currency-convertor /src/app.py
+++ level2/currency-convertor /src/app.py
@@ -1,8 +1,11 @@
 import streamlit as st
 from constants import CURRENCIES
-from currency_convertor import
+from currency_convertor import convert_currency, calculate_amount
 
 
+st.set_page_config(page_title="Currency Converter",
+                   layout= "centered",
+)
 
 st.title(':dollar: Currency Converter')
 
@@ -11,3 +14,10 @@ This tool allows you to instantly convert amounts between different currencies ð
 
 Enter the amount and choose the currencies to see the result.
 """)
+
+base_currency = st.selectbox("From Currency (base):", CURRENCIES)
+
+target_currency = st.selectbox("To Currency (target):", CURRENCIES) 
+
+amount = st.number_input("Amount to Convert:", min_value=0.0, value="min")
+
